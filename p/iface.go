ee08ac90b71344a1309ea299b80fc6f75f655c0e
Max Melentyev <max.melentyev@reddit.com>
2024-11-21 23:26:22 -0500

    Struct is inlined

--- p/iface.go
+++ p/iface.go
@@ -1,6 +1,13 @@
 package p
 
+import (
+	"mockery-issue/lib"
+)
+
 type T = int
+type S = lib.S
 
 //go:generate mockery --quiet --name I
-type I interface{ F(T) }
+type I interface {
+	F(T, S, lib.S)
+}
